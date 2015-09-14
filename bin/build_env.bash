98cc1088115b30c1b2c0540428996e93c1fb458f
Jukka Jylanki <jujjyl@gmail.com>
2015-09-14 15:40:21 +0300

    Update run_other_suite.bash

--- bin/build_env.bash
+++ bin/build_env.bash
@@ -27,3 +27,16 @@ echo "PATH:"
 echo $PATH
 echo "SPIDERMONKEY: "
 echo $SPIDERMONKEY
+
+echo "Currently checked out emscripten branch:"
+git log -n1
+
+echo "Currently checked out emscripten-fastcomp branch:"
+pushd ~/emslave/buildslave/$SLAVE_NAME/emsdk/clang/fastcomp/src
+git log -n1
+popd
+
+echo "Currently checked out emscripten-fastcomp-clang branch:"
+pushd ~/emslave/buildslave/$SLAVE_NAME/emsdk/clang/fastcomp/src/tools/clang
+git log -n1
+popd
--- bin/run_default_suite.bash
+++ bin/run_default_suite.bash
@@ -2,17 +2,4 @@
 
 build_env.bash
 
-echo "Currently checked out emscripten branch:"
-git log -n1
-
-echo "Currently checked out emscripten-fastcomp branch:"
-pushd ~/emslave/buildslave/$SLAVE_NAME/emsdk/clang/fastcomp/src
-git log -n1
-popd
-
-echo "Currently checked out emscripten-fastcomp-clang branch:"
-pushd ~/emslave/buildslave/$SLAVE_NAME/emsdk/clang/fastcomp/src/tools/clang
-git log -n1
-popd
-
 python tests/parallel_test_core.py ALL.test_hello_world
