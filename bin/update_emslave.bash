377f2b74c4676ad4eeb2da65a0007d238f36cd6c
Jukka Jylanki <jujjyl@gmail.com>
2015-09-14 16:45:52 +0300

    Set up first time run.

--- bin/update_emslave.bash
+++ bin/update_emslave.bash
@@ -7,7 +7,18 @@ fi
 
 cd ~/emslave
 git pull
- 
+
+if [ ! -d "~/emslave/buildslave/$SLAVE_NAME" ]; then
+	echo "ERROR: ~/emslave/buildslave/$SLAVE_NAME should exist at this point?!"
+	exit 1
+fi
+
+if [ ! -d "~/emslave/buildslave/$SLAVE_NAME/emsdk" ]; then
+	cd ~/emslave/buildslave/$SLAVE_NAME
+	git clone https://github.com/juj/emsdk/
+	cd emsdk
+fi
+
 cd ~/emslave/buildslave/$SLAVE_NAME/emsdk
 git pull
 ./emsdk install sdk-incoming-64bit
