ce2c20fdb0be4dd801eb78bd3a4c49636f3d151c
Jukka Jylänki <jujjyl@gmail.com>
2016-10-14 21:31:27 +0300

    Build Binaryen on all bots.

--- bin/update_emslave.bash
+++ bin/update_emslave.bash
@@ -36,14 +36,14 @@ git pull
 
 export CMAKE_CONFIG_TO_BUILD=""
 if [ "$(uname)" == "Darwin" ]; then # Mac OS X
-	export EMSDK_TARGETS="sdk-$TARGET_EMSCRIPTEN_BRANCH-64bit crunch-1.04"
+	export EMSDK_TARGETS="sdk-$TARGET_EMSCRIPTEN_BRANCH-64bit crunch-1.04 binaryen-master-64bit"
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then # Linux
-	export EMSDK_TARGETS="sdk-$TARGET_EMSCRIPTEN_BRANCH-64bit"
+	export EMSDK_TARGETS="sdk-$TARGET_EMSCRIPTEN_BRANCH-64bit binaryen-master-64bit"
 	# Override to build Release config because default RelWithDebInfo produces huge 7GB debugging symbols(!)
 	export CMAKE_CONFIG_TO_BUILD="--build=Release"
 	echo "We are on linux: building *Release* CMake configuration instead of the default one to avoid huge 7GB worth of debugging symbols being generated on disk, which exceeds total amount of RAM at link time on the Linux build slave."
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then # Windows Cygwin
-	export EMSDK_TARGETS="sdk-$TARGET_EMSCRIPTEN_BRANCH-64bit crunch-1.03 java-7.45-64bit spidermonkey-nightly-2015-04-12-64bit"
+	export EMSDK_TARGETS="sdk-$TARGET_EMSCRIPTEN_BRANCH-64bit crunch-1.03 java-7.45-64bit spidermonkey-nightly-2015-04-12-64bit binaryen-master-64bit"
 fi
 
 ./emsdk install --build-tests $CMAKE_CONFIG_TO_BUILD $EMSDK_TARGETS
