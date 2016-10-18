1c32385ef1fe616770df0c81dadb9628062e0381
Jukka Jylänki <jujjyl@gmail.com>
2016-10-18 13:36:00 +0300

    On OS X run Binaryen tests with Valgrind.

--- bin/run_binaryen_suite.bash
+++ bin/run_binaryen_suite.bash
@@ -12,7 +12,7 @@ echo $CWD
 
 if [ "$(uname)" == "Darwin" ]; then # Mac OS X
 	echo "Skipping GCC tests, they don't quite work on OS X at the moment."
-	python -u $SLAVE_ROOT/buildslave/$SLAVE_NAME/emsdk/binaryen/master/check.py --no-test-waterfall --no-abort-on-first-failure --no-run-gcc-tests
+	python -u $SLAVE_ROOT/buildslave/$SLAVE_NAME/emsdk/binaryen/master/check.py --no-test-waterfall --no-abort-on-first-failure --no-run-gcc-tests --valgrind=valgrind
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then # Linux
 	python -u $SLAVE_ROOT/buildslave/$SLAVE_NAME/emsdk/binaryen/master/check.py --no-test-waterfall --no-abort-on-first-failure
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then # Windows Cygwin
