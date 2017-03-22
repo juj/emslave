56f49deb2762af1d35c425d792a1837e75f00d1e
Jukka Jylänki <jujjyl@gmail.com>
2017-03-22 23:06:50 +0200

    Separate updating emslave and Emscripten to different scripts.

--- bin/update_emslave.bash
+++ bin/update_emslave.bash
@@ -12,10 +12,10 @@ if [ -z "$SLAVE_ROOT" ]; then
     exit 1
 fi
 
-if [ -z "$TARGET_EMSCRIPTEN_BRANCH" ]; then
-    echo "Need to set TARGET_EMSCRIPTEN_BRANCH env. var to either 'master' or 'incoming' before running update_emslave.bash!"
-    exit 1
-fi
+#if [ -z "$TARGET_EMSCRIPTEN_BRANCH" ]; then
+#    echo "Need to set TARGET_EMSCRIPTEN_BRANCH env. var to either 'master' or 'incoming' before running update_emslave.bash!"
+#    exit 1
+#fi
 
 cd $SLAVE_ROOT
 git pull
@@ -34,19 +34,19 @@ fi
 cd $SLAVE_ROOT/buildslave/$SLAVE_NAME/emsdk
 git pull
 
-export CMAKE_CONFIG_TO_BUILD=""
-if [ "$(uname)" == "Darwin" ]; then # Mac OS X
-	export EMSDK_TARGETS="sdk-$TARGET_EMSCRIPTEN_BRANCH-64bit crunch-1.04 binaryen-master-64bit"
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then # Linux
-	export EMSDK_TARGETS="sdk-$TARGET_EMSCRIPTEN_BRANCH-64bit binaryen-master-64bit"
-	# Override to build Release config because default RelWithDebInfo produces huge 7GB debugging symbols(!)
-	export CMAKE_CONFIG_TO_BUILD="--build=Release"
-	echo "We are on linux: building *Release* CMake configuration instead of the default one to avoid huge 7GB worth of debugging symbols being generated on disk, which exceeds total amount of RAM at link time on the Linux build slave."
-elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then # Windows Cygwin
-	export EMSDK_TARGETS="sdk-$TARGET_EMSCRIPTEN_BRANCH-64bit crunch-1.03 java-7.45-64bit spidermonkey-nightly-2015-04-12-64bit binaryen-master-64bit"
-fi
+#export CMAKE_CONFIG_TO_BUILD=""
+#if [ "$(uname)" == "Darwin" ]; then # Mac OS X
+#	export EMSDK_TARGETS="sdk-$TARGET_EMSCRIPTEN_BRANCH-64bit crunch-1.04 binaryen-master-64bit"
+#elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then # Linux
+#	export EMSDK_TARGETS="sdk-$TARGET_EMSCRIPTEN_BRANCH-64bit binaryen-master-64bit"
+#	# Override to build Release config because default RelWithDebInfo produces huge 7GB debugging symbols(!)
+#	export CMAKE_CONFIG_TO_BUILD="--build=Release"
+#	echo "We are on linux: building *Release* CMake configuration instead of the default one to avoid huge 7GB worth of debugging symbols being generated on disk, which exceeds total amount of RAM at link time on the Linux build slave."
+#elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then # Windows Cygwin
+#	export EMSDK_TARGETS="sdk-$TARGET_EMSCRIPTEN_BRANCH-64bit crunch-1.03 java-7.45-64bit spidermonkey-nightly-2015-04-12-64bit binaryen-master-64bit"
+#fi
 
-./emsdk install --build-tests $CMAKE_CONFIG_TO_BUILD $EMSDK_TARGETS
+#./emsdk install --build-tests $CMAKE_CONFIG_TO_BUILD $EMSDK_TARGETS
 
 rm -f ~/.emscripten || true
 ./emsdk activate --embedded $EMSDK_TARGETS
--- bin/update_emslave_and_emscripten.bat
+++ bin/update_emslave_and_emscripten.bat
@@ -2,7 +2,7 @@
 
 IF "%SLAVE_NAME%"=="" (GOTO error_no_slave_name)
 IF "%SLAVE_ROOT%"=="" (GOTO error_no_slave_root)
-:: IF "%TARGET_EMSCRIPTEN_BRANCH%"=="" (GOTO error_no_emscripten_branch)
+IF "%TARGET_EMSCRIPTEN_BRANCH%"=="" (GOTO error_no_emscripten_branch)
 
 cd "%SLAVE_ROOT"
 call git pull
@@ -18,10 +18,10 @@ cd emsdk
 cd %SLAVE_ROOT%\buildslave\%SLAVE_NAME%\emsdk
 call git pull
 
-:: set EMSDK_TARGETS=sdk-%TARGET_EMSCRIPTEN_BRANCH%-64bit crunch-1.03 java-7.45-64bit spidermonkey-nightly-2015-04-12-64bit gnu-2.5.4 binaryen-master-64bit
-:: call emsdk install --vs2015 --build-tests %EMSDK_TARGETS%
-:: del %userprofile%\.emscripten
-:: call emsdk activate --vs2015 --embedded %EMSDK_TARGETS%
+set EMSDK_TARGETS=sdk-%TARGET_EMSCRIPTEN_BRANCH%-64bit crunch-1.03 java-7.45-64bit spidermonkey-nightly-2015-04-12-64bit gnu-2.5.4 binaryen-master-64bit
+call emsdk install --vs2015 --build-tests %EMSDK_TARGETS%
+del %userprofile%\.emscripten
+call emsdk activate --vs2015 --embedded %EMSDK_TARGETS%
 
 echo "This file should not be read, nothing but errors here!" > %userprofile%\.emscripten
 
