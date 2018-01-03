f41b9654e3614bbba9c05f8e0124d22a0f49f7ec
Jukka Jylänki <jujjyl@gmail.com>
2018-01-03 21:40:20 +0200

    Pass args to force-disable futimens() and build with OS X SDK 10.11

--- bin/start_slave.bash
+++ bin/start_slave.bash
@@ -5,5 +5,13 @@ if [ -z "$SLAVE_ROOT" ]; then
     exit 1
 fi
 
+if [ "$(uname)" == "Darwin" ]; then # Mac OS X
+	export MACOSX_DEPLOYMENT_TARGET=10.11
+	echo MACOSX_DEPLOYMENT_TARGET=$MACOSX_DEPLOYMENT_TARGET
+
+	export LLVM_CMAKE_ARGS=-DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.11.sdk,-DHAVE_FUTIMENS=0,-DCMAKE_MACOSX_DEPLOYMENT_TARGET=10.11
+	echo LLVM_CMAKE_ARGS=$LLVM_CMAKE_ARGS
+fi
+
 cd $SLAVE_ROOT/buildslave
 buildslave start
