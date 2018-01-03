e5bd93422079804ab6c471f4f92d4e103cbfba12
Jukka Jylänki <jujjyl@gmail.com>
2018-01-03 22:09:58 +0200

    Fix typo in CMAKE_OSX_DEPLOYMENT_TARGET variable name

--- bin/start_slave.bash
+++ bin/start_slave.bash
@@ -9,7 +9,7 @@ if [ "$(uname)" == "Darwin" ]; then # Mac OS X
 	export MACOSX_DEPLOYMENT_TARGET=10.11
 	echo MACOSX_DEPLOYMENT_TARGET=$MACOSX_DEPLOYMENT_TARGET
 
-	export LLVM_CMAKE_ARGS=-DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.11.sdk,-DHAVE_FUTIMENS=0,-DCMAKE_MACOSX_DEPLOYMENT_TARGET=10.11
+	export LLVM_CMAKE_ARGS=-DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.11.sdk,-DHAVE_FUTIMENS=0,-DCMAKE_OSX_DEPLOYMENT_TARGET=10.11
 	echo LLVM_CMAKE_ARGS=$LLVM_CMAKE_ARGS
 fi
 
