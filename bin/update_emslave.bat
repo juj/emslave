95aa738b9ee8b844b5533361c0e7e1898f9f2945
Jukka Jylänki <jujjyl@gmail.com>
2018-02-02 22:23:44 +0200

    Skip "emsdk activate" call in update_emslave.bash, as it is done in later update_emslave_and_emscripten.bash script when we know which emsdk packages to update.

--- bin/update_emslave.bat
+++ bin/update_emslave.bat
@@ -19,11 +19,6 @@ cd %SLAVE_ROOT%\buildslave\%SLAVE_NAME%\emsdk
 call git checkout -- emscripten-tags.txt binaryen-tags.txt
 call git pull
 
-:: set EMSDK_TARGETS=sdk-%TARGET_EMSCRIPTEN_BRANCH%-64bit crunch-1.03 java-8.152-64bit spidermonkey-nightly-2015-04-12-64bit gnu-2.5.4 binaryen-master-64bit
-:: call emsdk install --vs2015 --build-tests %EMSDK_TARGETS%
-:: del %userprofile%\.emscripten
-:: call emsdk activate --vs2015 --embedded %EMSDK_TARGETS%
-
 echo "This file should not be read, nothing but errors here!" > %userprofile%\.emscripten
 
 IF "%EMSCRIPTEN_TEMP%"=="" (GOTO no_emscripten_temp_override)
